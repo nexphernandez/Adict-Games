6f8a58b3c5f7b2fab40d815d7564a261b17e6a40
nexphernandez <nicolaseh3b1112@gmail.com>
2025-04-04 18:26:02 +0100

    Se aniade la clase ConfigProperties #10

--- src/main/java/backend/es/nexphernandez/adict/games/config/ConfigManager.java
+++ src/main/java/backend/es/nexphernandez/adict/games/config/ConfigManager.java
@@ -1,5 +1,37 @@
 package backend.es.nexphernandez.adict.games.config;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 public class ConfigManager {
+    public static class ConfigProperties {
     
+        static String path;
+
+        private static final Properties properties = new Properties();
+
+        /**
+         * Metodo estatico para obtener una propiedad
+         */
+        public static String getProperty(String key){
+            return properties.getProperty(key);
+        }
+
+        public static void setPath(String rutaPath){
+            File file = new File(rutaPath);
+            if (!file.exists() || !file.isFile()) {
+                System.out.println("Path:"+file.getAbsolutePath());
+            }
+            path = rutaPath;
+            try {
+                FileInputStream input = new FileInputStream(path);
+                InputStreamReader isr = new InputStreamReader(input, "UTF-8");
+                properties.load(isr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/backend/es/nexphernandez/adict/games/controller/PrincipalController.java
+++ src/main/java/backend/es/nexphernandez/adict/games/controller/PrincipalController.java
@@ -1,5 +1,9 @@
 package backend.es.nexphernandez.adict.games.controller;
+import java.util.ArrayList;
+import java.util.List;
+
 import backend.es.nexphernandez.adict.games.abstractas.AbstractController;
+import backend.es.nexphernandez.adict.games.config.ConfigManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +14,18 @@ import javafx.stage.Stage;
  * @version 1.0.0
  */
 public class PrincipalController extends AbstractController{
-    private final String pathFichero="src/main/resources/";
+    private final String pathFichero="src/main/resources/idiomas";
+    private final String ficheroStr= "idioma-";
 
+    public void initialize(){
+        List<String> listaIdiomas = new ArrayList<>();
+        listaIdiomas.add("es");
+        listaIdiomas.add("en");
+        cargarIdioma("es");
+    }
+
+    private void cargarIdioma(String idioma){
+        String path = pathFichero+ficheroStr+idioma+".properties";
+        ConfigManager.ConfigProperties.setPath(path);
+    }
 }
